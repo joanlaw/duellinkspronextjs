b62f7319479291bef1df69aa3cdfe108a7efffa7
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2022-11-27 19:59:04 -0600

    optimizacion SEO

--- pages/decks-meta/index.js
+++ pages/decks-meta/index.js
@@ -1,9 +1,15 @@
 import Clustersmeta from "../../components/Clustersmeta"
 import Header from "../../components/Header"
 import Footer from "../../components/Footer"
+import Head from "next/head"
 
 export default function index() {
   return (
+    <>
+    <Head>
+    <title>Decks Meta</title>
+  <meta name='Tier List' content='Decks meta o Tier List basados en criterios de la comunidad y los decks o mazos más ganadores' />
+    </Head>
     <div>
     <Header />
     <h1 className='h1margen'>Tier List</h1>
@@ -12,5 +18,6 @@ export default function index() {
     <br />
     <Footer />
 </div>
+</>
   )
 }
