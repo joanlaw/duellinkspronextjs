51ad66b2daea83e703ba4e9853d7741c363cacbb
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2023-05-08 13:07:58 -0600

    decks-meta

--- pages/torneos/index.js
+++ pages/torneos/index.js
@@ -8,7 +8,10 @@ export default function torneos() {
 
     //HOOKS - CAPTURA ESTADOS
     
-    const [torneos, setTorneos] = useState ([])
+  //  const [torneos, setTorneos] = useState ([])
+
+// {torneos.length === 0 ? (
+   
 
 
   return (
@@ -17,17 +20,7 @@ export default function torneos() {
         <div className='container'>
             <br />
             <h1>Torneos</h1>
-      {torneos.length === 0 ? (
-        <p>PRONTO TENDREMOS REPORTES DE TORNEOS DISPONIBLES GRACIAS POR VISITAR NUESTRA WEB.</p>
-      ) : (
-        torneos.map((torneo) => (
-          <div key={torneo.id}>
-            <h2>{torneo.nombre}</h2>
-            <p>{torneo.descripcion}</p>
-            {/* Agrega más información del torneo aquí */}
-          </div>
-        ))
-      )}
+            <p>PRONTO TENDREMOS REPORTES DE TORNEOS DISPONIBLES GRACIAS POR VISITAR NUESTRA WEB.</p>
       <br />
       <br />
       <br />
