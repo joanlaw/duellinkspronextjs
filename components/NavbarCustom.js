f0df591f72f1734ec344685a9c0661836b19aedd
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2023-08-16 01:13:18 -0600

    dlfkdlfkdlk

--- components/NavbarCustom.js
+++ components/NavbarCustom.js
@@ -12,6 +12,7 @@ import {
   NavbarMenu,
   NavbarMenuToggle,
   useToasts,
+  Input
 } from '@nextui-org/react';
 import { SearchIcon } from "./SearchIcon";
 import { AcmeLogo } from "./AcmeLogo";
@@ -114,7 +115,18 @@ export default function NavbarCustom() {
       </NavbarContent>
 
       <NavbarContent as="div" className="items-center" justify="end">
-        {/*...otros elementos del Navbar...*/}
+      <Input
+          classNames={{
+            base: "max-w-full sm:max-w-[10rem] h-10",
+            mainWrapper: "h-full",
+            input: "text-small",
+            inputWrapper: "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+          }}
+          placeholder="Type to search..."
+          size="sm"
+          startContent={<SearchIcon size={18} />}
+          type="search"
+        />
 
         <Dropdown placement="bottom-end">
           <DropdownTrigger>
@@ -143,7 +155,15 @@ export default function NavbarCustom() {
               <p className="font-semibold">{userImage ? "Logged in as" : "Login"}</p>
               <p className="font-semibold">{userImage ? username : "Login"}</p>
               </DropdownItem>
-              {/*... otros elementos del menú ... */}
+              <DropdownItem key="settings">My Settings</DropdownItem>
+            <DropdownItem key="team_settings">Team Settings</DropdownItem>
+            <DropdownItem key="analytics">Analytics</DropdownItem>
+            <DropdownItem key="system">System</DropdownItem>
+            <DropdownItem key="configurations">Configurations</DropdownItem>
+            <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
+            <DropdownItem key="logout" color="danger">
+              Log Out
+              </DropdownItem>
             </DropdownMenu>
           ) : (
             <DropdownMenu aria-label="Login" variant="flat">
