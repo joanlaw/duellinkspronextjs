a637b3a3e2701be3670595090fc3a91ba5f004f0
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2023-08-17 01:17:29 -0600

    autenticacion jwt

--- components/NavbarCustom.js
+++ components/NavbarCustom.js
@@ -1,179 +1,178 @@
-import React, { useState, useEffect } from "react";
-import {
-  Navbar,
-  NavbarContent,
-  NavbarBrand,
-  NavbarItem,
-  Avatar,
-  Dropdown,
-  DropdownMenu,
-  DropdownItem,
-  DropdownTrigger,
-  NavbarMenu,
-  NavbarMenuToggle,
-  useToasts,
-  Input
-} from '@nextui-org/react';
-import { SearchIcon } from "./SearchIcon";
-import { AcmeLogo } from "./AcmeLogo";
-import Link from 'next/link';
-import { parse } from 'cookie';
+  import React, { useState, useEffect } from "react";
+  import {
+    Navbar,
+    NavbarContent,
+    NavbarBrand,
+    NavbarItem,
+    Avatar,
+    Dropdown,
+    DropdownMenu,
+    DropdownItem,
+    DropdownTrigger,
+    NavbarMenu,
+    NavbarMenuToggle,
+    useToasts,
+    Input
+  } from '@nextui-org/react';
+  import { SearchIcon } from "./SearchIcon";
+  import { AcmeLogo } from "./AcmeLogo";
+  import Link from 'next/link';
+  import { parse } from 'cookie';
 
-export default function NavbarCustom() {
-  const [userImage, setUserImage] = useState(null);
-  const [authenticated, setAuthenticated] = useState(false);
-  const [username, setUsername] = useState(null);
+  export default function NavbarCustom() {
+    const [userImage, setUserImage] = useState(null);
+    const [authenticated, setAuthenticated] = useState(false);
+    const [username, setUsername] = useState(null);
 
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
 
-  useEffect(() => {
-    const cookies = parse(document.cookie);
-    console.log("Cookies:", cookies);
 
-    fetch("https://api.duellinks.pro/get-user-info/", {
-      credentials: 'include'
-    })
-      .then((response) => {
-        console.log("Response headers:", response.headers);
-        return response.json();
+    useEffect(() => {
+      // Eliminamos el código de parse de cookies, ya que no estamos usando cookies para autenticación JWT
+  
+      fetch("https://api.duellinks.pro/get-user-info/", {
+        credentials: 'include'
       })
-      .then((data) => {
-        console.log("User info response:", data);
-        setUserImage(data.authenticated ? data.image : null);
-        setAuthenticated(data.authenticated);
-        setUsername(data.username); // <-- Añade esta línea
-      })
-      .catch((error) => {
-        console.error("Error fetching user info:", error);
-      });
-  }, []);
+        .then((response) => {
+          console.log("Response headers:", response.headers);
+          return response.json();
+        })
+        .then((data) => {
+          console.log("User info response:", data);
+          setUserImage(data.authenticated ? data.image : null);
+          setAuthenticated(data.authenticated);
+          setUsername(data.username);
+        })
+        .catch((error) => {
+          console.error("Error fetching user info:", error);
+        });
+    }, []);
+  
+    const handleLogin = () => {
+      window.location.href = "https://api.duellinks.pro/login";
+    };
+    return (
+      <Navbar isBordered className="bg-[#0a141d]">
+        <NavbarContent className="sm:hidden" justify="start">
+          <NavbarMenuToggle
+            aria-label="Open Menu"
+            active={isMenuOpen}
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          />
+        </NavbarContent>
 
-  const handleLogin = () => {
-    window.location.href = "https://api.duellinks.pro/login";
-  };
+        <NavbarContent className="sm:hidden pr-3" justify="center">
+          <NavbarBrand className="mr-4">
+            <AcmeLogo />
+            <Link href="/">
+              <p className="font-bold text-inherit">DUEL LINKS PRO</p>
+            </Link>
+          </NavbarBrand>
+        </NavbarContent>
 
-  return (
-    <Navbar isBordered className="bg-[#0a141d]">
-      <NavbarContent className="sm:hidden" justify="start">
-        <NavbarMenuToggle
-          aria-label="Open Menu"
-          active={isMenuOpen}
-          onClick={() => setIsMenuOpen(!isMenuOpen)}
-        />
-      </NavbarContent>
+        <NavbarMenu active={isMenuOpen} onClose={() => setIsMenuOpen(false)}>
+          <NavbarItem>
+            <Link href="/mazos">
+              Decks
+            </Link>
+          </NavbarItem>
+          <NavbarItem isActive>
+            <Link href="/torneos">
+              Torneos
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link href="/cards">
+              Cartas
+            </Link>
+          </NavbarItem>
+        </NavbarMenu>
 
-      <NavbarContent className="sm:hidden pr-3" justify="center">
-        <NavbarBrand className="mr-4">
-          <AcmeLogo />
-          <Link href="/">
-            <p className="font-bold text-inherit">DUEL LINKS PRO</p>
-          </Link>
-        </NavbarBrand>
-      </NavbarContent>
+        <NavbarContent className="hidden sm:flex flex-grow gap-4 justify-between">
+          <NavbarBrand className="mr-4">
+            <AcmeLogo />
+            <Link href="/">
+              <p className="font-bold text-inherit">DUEL LINKS PRO</p>
+            </Link>
+          </NavbarBrand>
+          <NavbarItem>
+            <Link href="/mazos">
+              Decks
+            </Link>
+          </NavbarItem>
+          <NavbarItem isActive>
+            <Link href="/torneos">
+              Torneos
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link href="/cards">
+              Cartas
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
 
-      <NavbarMenu active={isMenuOpen} onClose={() => setIsMenuOpen(false)}>
-        <NavbarItem>
-          <Link href="/mazos">
-            Decks
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="/torneos">
-            Torneos
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link href="/cards">
-            Cartas
-          </Link>
-        </NavbarItem>
-      </NavbarMenu>
+        <NavbarContent as="div" className="items-center" justify="end">
+        <Input
+            classNames={{
+              base: "max-w-full sm:max-w-[10rem] h-10",
+              mainWrapper: "h-full",
+              input: "text-small",
+              inputWrapper: "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+            }}
+            placeholder="Type to search..."
+            size="sm"
+            startContent={<SearchIcon size={18} />}
+            type="search"
+          />
 
-      <NavbarContent className="hidden sm:flex flex-grow gap-4 justify-between">
-        <NavbarBrand className="mr-4">
-          <AcmeLogo />
-          <Link href="/">
-            <p className="font-bold text-inherit">DUEL LINKS PRO</p>
-          </Link>
-        </NavbarBrand>
-        <NavbarItem>
-          <Link href="/mazos">
-            Decks
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="/torneos">
-            Torneos
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link href="/cards">
-            Cartas
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
+          <Dropdown placement="bottom-end">
+            <DropdownTrigger>
+              {authenticated ? (
+                <div className="flex gap-4 items-center">
+                <Avatar
+                  isBordered radius="full"
+                  className="flex gap-4 items-center cursor-pointer"
+                  src={userImage}
+                />
+                </div>
+              ) : (
+                <div className="flex gap-4 items-center cursor-pointer">
+                <Avatar
+                  isBordered radius="full"
+                  className="flex gap-4 items-center"
+                  src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                />
+                </div>
+              )}
+            </DropdownTrigger>
 
-      <NavbarContent as="div" className="items-center" justify="end">
-      <Input
-          classNames={{
-            base: "max-w-full sm:max-w-[10rem] h-10",
-            mainWrapper: "h-full",
-            input: "text-small",
-            inputWrapper: "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
-          }}
-          placeholder="Type to search..."
-          size="sm"
-          startContent={<SearchIcon size={18} />}
-          type="search"
-        />
-
-        <Dropdown placement="bottom-end">
-          <DropdownTrigger>
             {authenticated ? (
-               <div className="flex gap-4 items-center">
-              <Avatar
-                isBordered radius="full"
-                className="flex gap-4 items-center cursor-pointer"
-                src={userImage}
-              />
-              </div>
+              <DropdownMenu aria-label="Profile Actions" variant="flat">
+                <DropdownItem key="profile" className="h-14 gap-2">
+                <p className="font-semibold">{userImage ? "Logged in as" : "Login"}</p>
+                <p className="font-semibold">{userImage ? username : "Login"}</p>
+                </DropdownItem>
+                <DropdownItem key="settings">My Settings</DropdownItem>
+              <DropdownItem key="team_settings">Team Settings</DropdownItem>
+              <DropdownItem key="analytics">Analytics</DropdownItem>
+              <DropdownItem key="system">System</DropdownItem>
+              <DropdownItem key="configurations">Configurations</DropdownItem>
+              <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
+              <DropdownItem key="logout" color="danger">
+                Log Out
+                </DropdownItem>
+              </DropdownMenu>
             ) : (
-              <div className="flex gap-4 items-center cursor-pointer">
-              <Avatar
-                isBordered radius="full"
-                className="flex gap-4 items-center"
-                src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-              />
-              </div>
+              <DropdownMenu aria-label="Login" variant="flat">
+                <DropdownItem key="login" onClick={handleLogin}>
+                  Login
+                </DropdownItem>
+              </DropdownMenu>
             )}
-          </DropdownTrigger>
-
-          {authenticated ? (
-            <DropdownMenu aria-label="Profile Actions" variant="flat">
-              <DropdownItem key="profile" className="h-14 gap-2">
-              <p className="font-semibold">{userImage ? "Logged in as" : "Login"}</p>
-              <p className="font-semibold">{userImage ? username : "Login"}</p>
-              </DropdownItem>
-              <DropdownItem key="settings">My Settings</DropdownItem>
-            <DropdownItem key="team_settings">Team Settings</DropdownItem>
-            <DropdownItem key="analytics">Analytics</DropdownItem>
-            <DropdownItem key="system">System</DropdownItem>
-            <DropdownItem key="configurations">Configurations</DropdownItem>
-            <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
-            <DropdownItem key="logout" color="danger">
-              Log Out
-              </DropdownItem>
-            </DropdownMenu>
-          ) : (
-            <DropdownMenu aria-label="Login" variant="flat">
-              <DropdownItem key="login" onClick={handleLogin}>
-                Login
-              </DropdownItem>
-            </DropdownMenu>
-          )}
-        </Dropdown>
-      </NavbarContent>
-    </Navbar>
-  );
-}
+          </Dropdown>
+        </NavbarContent>
+      </Navbar>
+    );
+  }
