bb1c06107f5fdce755b5a5bb0b48ad0affead477
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2023-08-19 17:43:34 -0600

    token update

--- contexts/UserContext.js
+++ contexts/UserContext.js
@@ -6,46 +6,67 @@ export const UserProvider = ({ children }) => {
     const [userImage, setUserImage] = useState(null);
     const [authenticated, setAuthenticated] = useState(false);
     const [username, setUsername] = useState(null);
+    const [token, setToken] = useState(null);
+
 
     useEffect(() => {
         const urlParams = new URLSearchParams(window.location.search);
         const tokenFromUrl = urlParams.get('token');
-        const token = tokenFromUrl || localStorage.getItem("token");
+        const localToken = tokenFromUrl || localStorage.getItem("token");
     
         if (tokenFromUrl) {
-          localStorage.setItem("token", tokenFromUrl);
+            localStorage.setItem("token", tokenFromUrl);
+            setToken(tokenFromUrl);  // Actualizar el estado del token
+        } else {
+            setToken(localToken);    // Usar el token del localStorage si no hay token en la URL
         }
     
-        if (token) {
-          fetch("https://api.duellinks.pro/get-user-info/", {
-            credentials: "include",
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          })
-          .then((response) => response.json())
-          .then((data) => {
-            setUserImage(data.authenticated ? data.image : null);
-            setAuthenticated(data.authenticated);
-            setUsername(data.username);
-          })
-          .catch((error) => {
-            localStorage.removeItem("token");
-            setAuthenticated(false);
-          });
+        if (localToken) {
+            fetch("https://api.duellinks.pro/get-user-info/", {
+                credentials: "include",
+                headers: {
+                    Authorization: `Bearer ${localToken}`,
+                },
+            })
+            .then((response) => response.json())
+            .then((data) => {
+                setUserImage(data.authenticated ? data.image : null);
+                setAuthenticated(data.authenticated);
+                setUsername(data.username);
+            })
+            .catch((error) => {
+                localStorage.removeItem("token");
+                setAuthenticated(false);
+            });
         } else {
-          setAuthenticated(false);
+            setAuthenticated(false);
         }
-      }, []);
-
-    const handleLogin = () => {
-        window.location.href = "https://api.duellinks.pro/login";
+    }, []);
+    
+      const handleLogin = () => {
+        // Abrir una nueva ventana para el inicio de sesión.
+        const loginWindow = window.open("https://api.duellinks.pro/login", "DuelLinksLogin", "width=500,height=500");
+        
+        // Monitorear si la ventana se cierra
+        const checkWindowClosed = setInterval(() => {
+            if (loginWindow.closed) {
+                clearInterval(checkWindowClosed);
+                
+                // Una vez que la ventana se cierra, verifica si el usuario se autenticó.
+                const token = localStorage.getItem("token");
+                if (token) {
+                    // Continuar con el flujo después de la autenticación (por ejemplo, completar la inscripción)
+                    console.log("Usuario autenticado");
+                }
+            }
+        }, 100);
     };
+    
 
     return (
-        <UserContext.Provider value={{ userImage, authenticated, username, handleLogin }}>
-            {children}
-        </UserContext.Provider>
+        <UserContext.Provider value={{ userImage, authenticated, username, handleLogin, token }}>
+        {children}
+    </UserContext.Provider>
     );
 };
 
