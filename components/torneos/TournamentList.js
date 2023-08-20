d2a6f495e7195d55e9fd5a71a55cb60443365c3e
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2023-08-19 23:28:12 -0600

    kdlfkl

--- components/torneos/TournamentList.js
+++ components/torneos/TournamentList.js
@@ -1,36 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 const TournamentList = ({ discordId }) => {
-  const [tournaments, setTournaments] = useState([]);
-  const [loading, setLoading] = useState(true);
+    const [tournaments, setTournaments] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchTournaments = async () => {
-      setLoading(true);
-      try {
-        const response = await axios.get(`https://api.duellinks.pro/leagues/discordId/${discordId}`);
-        setTournaments(response.data);
-      } catch (error) {
-        console.error('Hubo un problema al cargar la lista de torneos:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
+    useEffect(() => {
+        const fetchTournaments = async () => {
+            setLoading(true);
+            try {
+                const response = await axios.get(`https://api.duellinks.pro/leagues/discordId/${discordId}`);
+                setTournaments(response.data);
+            } catch (error) {
+                console.error('Hubo un problema al cargar la lista de torneos:', error);
+            } finally {
+                setLoading(false);
+            }
+        };
 
-    fetchTournaments();
-  }, [discordId]);
+        fetchTournaments();
+    }, [discordId]);
 
-  if (loading) {
-    return <div>Cargando torneos...</div>;
-  }
+    if (loading) {
+        return <div>Cargando torneos...</div>;
+    }
 
-  return (
-    <div>
-      <h2>Lista de Torneos</h2>
-      {tournaments.map((tournament, index) => (
-        <div key={index}>
-          <h3>{tournament.league_name}</h3>
-          {/* Aquí puedes agregar más detalles del torneo */}
+    return (
+        <div>
+            <h2>Lista de Torneos</h2>
+            {tournaments.map((tournament, index) => (
+                <div key={index}>
+                    <h3>{tournament.league_name}</h3>
+                    {/* Aquí puedes agregar más detalles del torneo */}
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
+
+export default TournamentList;
