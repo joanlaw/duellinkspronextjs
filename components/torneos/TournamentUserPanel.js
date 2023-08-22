0334e37d47a17727391dc8c7637dedf5a238e17e
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2023-08-22 01:09:49 -0600

    lkdlfklk

--- components/torneos/TournamentUserPanel.js
+++ components/torneos/TournamentUserPanel.js
@@ -105,56 +105,57 @@ function TournamentUserPanel({ onClose, leagueId }) {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-75">
-      <div className="bg-white p-6 rounded-md shadow-lg text-black">
-        <h2 className="text-xl font-semibold mb-4">Torneos en el campeonato</h2>
-        {loading ? (
-          <p>Cargando torneos...</p>
-        ) : (
-          <>
-            <ul className="list-disc pl-6 mb-4">
-              {tournaments.map((tournament) => (
-                <li key={tournament._id}>{tournament.league_name}</li>
+    <div className="bg-white p-6 rounded-md shadow-lg text-black">
+      <h2 className="text-xl font-semibold mb-4">Torneos en el campeonato</h2>
+      {loading ? (
+        <p>Cargando torneos...</p>
+      ) : (
+        <>
+          <ul className="list-disc pl-6 mb-4">
+            {tournaments.map((tournament) => (
+              <li key={tournament._id}>{tournament.league_name}</li>
+            ))}
+          </ul>
+          
+          {playerDeck ? (
+            <div>
+              <h3>Previsualización de tu mazo:</h3>
+              {["main_deck", "extra_deck", "side_deck", "especial_deck"].map(deckType => (
+                <div className="mb-4" key={deckType}>
+                  <label className="block mb-2">{deckType.replace("_", " ").toUpperCase()}:</label>
+                  {getImagePreview(deckType) && (
+                    <img src={getImagePreview(deckType)} alt="Preview" className="mb-2" />
+                  )}
+                </div>
               ))}
-            </ul>
+            </div>
+          ) : (
             <form onSubmit={handleSubmit}>
-  <div className="mb-4">
-    <label htmlFor="main_deck" className="block mb-2">Main Deck:</label>
-    <input type="file" name="main_deck" onChange={handleImageChange} />
-  </div>
-  <div className="mb-4">
-    <label htmlFor="extra_deck" className="block mb-2">Extra Deck:</label>
-    <input type="file" name="extra_deck" onChange={handleImageChange} />
-  </div>
-  <div className="mb-4">
-    <label htmlFor="side_deck" className="block mb-2">Side Deck:</label>
-    <input type="file" name="side_deck" onChange={handleImageChange} />
-  </div>
-  <div className="mb-4">
-    <label htmlFor="especial_deck" className="block mb-2">Especial Deck:</label>
-    <input type="file" name="especial_deck" onChange={handleImageChange} />
-  </div>
-  <div className="mb-4">
-  <label htmlFor="main_deck" className="block mb-2">Main Deck:</label>
-  {getImagePreview('main_deck') && (
-    <img src={getImagePreview('main_deck')} alt="Preview" className="mb-2" />
-  )}
-  <input type="file" name="main_deck" onChange={handleImageChange} />
-</div>
-  <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-    Subir Imágenes
-  </button>
-</form>
-
-            <button
-              className="mt-4 px-4 py-2 bg-gray-700 text-white rounded hover:bg-gray-800"
-              onClick={onClose}
-            >
-              Cerrar
-            </button>
-          </>
-        )}
-      </div>
+              {["main_deck", "extra_deck", "side_deck", "especial_deck"].map(deckType => (
+                <div className="mb-4" key={deckType}>
+                  <label htmlFor={deckType} className="block mb-2">{deckType.replace("_", " ").toUpperCase()}:</label>
+                  {getImagePreview(deckType) && (
+                    <img src={getImagePreview(deckType)} alt="Preview" className="mb-2" />
+                  )}
+                  <input type="file" name={deckType} onChange={handleImageChange} />
+                </div>
+              ))}
+              <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+                Subir Imágenes
+              </button>
+            </form>
+          )}
+          
+          <button
+            className="mt-4 px-4 py-2 bg-gray-700 text-white rounded hover:bg-gray-800"
+            onClick={onClose}
+          >
+            Cerrar
+          </button>
+        </>
+      )}
     </div>
+  </div>
   );
 }
 
