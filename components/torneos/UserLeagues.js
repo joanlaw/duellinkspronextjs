5da1575702c9257cb12123a2af03347e2bbe76ab
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2023-08-23 17:17:12 -0600

    lkdlfklk

--- components/torneos/UserLeagues.js
+++ components/torneos/UserLeagues.js
@@ -30,21 +30,51 @@ function UserLeagues() {
         }
     }, [discordId, authenticated]);
 
+    const updateLeagues = async () => {
+      try {
+        const res = await axios.get(`https://api.duellinks.pro/leagues/organizer/${discordId}`);
+        setLeagues(res.data || []);
+      } catch (error) {
+        console.error("Error al actualizar las ligas:", error);
+      }
+    };
+
+    const startTournament = async (leagueId) => {
+        try {
+            await axios.post(`https://api.duellinks.pro/leagues/${leagueId}/start-tournament`);
+            updateLeagues();
+        } catch (error) {
+            console.error("Error al iniciar el torneo:", error);
+        }
+    };
+
+    const startNextRound = async (leagueId) => {
+        try {
+            await axios.post(`https://api.duellinks.pro/leagues/${leagueId}/start-next-round`);
+            updateLeagues();
+        } catch (error) {
+            console.error("Error al iniciar la siguiente ronda:", error);
+        }
+    };
+
     return (
-        <div className="container mx-auto mt-10 mb-10 p-6 rounded-md shadow-sm" style={{ backgroundColor: '#27272a' }}>
-        <h2 className="text-2xl font-bold mb-4 text-white">Mis torneos y ligas creados</h2>
-        {leagues.map(league => (
-          <div key={league._id} className="bg-white p-4 rounded-md mb-4 shadow-md text-black">
-            <h3 className="text-xl font-medium mb-2">{league.league_name}</h3>
-            <p className="mb-1">Formato: {league.league_format}</p>
-            <p>Fecha de inicio: {new Date(league.start_date).toLocaleDateString()}</p>
-            <button onClick={() => openAdminPanel(league._id)}>Administrar Torneo</button>
-          </div>
-        ))}
-        {selectedLeague && (
-          <TournamentAdminPanel leagueId={selectedLeague} onClose={closeAdminPanel} />
-        )}
-      </div>
+      <div className="container mx-auto mt-10 mb-10 p-6 rounded-md shadow-sm" style={{ backgroundColor: '#27272a' }}>
+      <h2 className="text-2xl font-bold mb-4 text-white">Mis torneos y ligas creados</h2>
+      {leagues.map(league => (
+        <div key={league._id} className="bg-white p-4 rounded-md mb-4 shadow-md text-black">
+          <h3 className="text-xl font-medium mb-2">{league.league_name}</h3>
+          <p className="mb-1">Formato: {league.league_format}</p>
+          <p>Ronda actual: {league.current_round || "Torneo no iniciado"}</p>
+          <p>Fecha de inicio: {new Date(league.start_date).toLocaleDateString()}</p>
+          <button onClick={() => openAdminPanel(league._id)}>Administrar Torneo</button>
+          <button onClick={() => startTournament(league._id)}>Iniciar Torneo</button>
+          <button onClick={() => startNextRound(league._id)}>Iniciar Siguiente Ronda</button>
+        </div>
+      ))}
+      {selectedLeague && (
+        <TournamentAdminPanel leagueId={selectedLeague} onClose={closeAdminPanel} />
+      )}
+    </div>
     );
 }
 
