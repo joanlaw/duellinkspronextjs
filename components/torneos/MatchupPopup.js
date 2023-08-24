6de7f5f8f111236f5414bef043f02a952820e9fb
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2023-08-23 22:57:32 -0600

    kldkflk

--- components/torneos/MatchupPopup.js
+++ components/torneos/MatchupPopup.js
@@ -4,11 +4,11 @@ function MatchupPopup({ matches = [], onClose }) {
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50 bg-opacity-50 bg-black">
       <div className="bg-white rounded-lg p-8 w-full md:w-2/3 lg:w-1/2">
-        <h2 className="text-2xl mb-4">Emparejamientos de la Ronda Actual</h2>
-        <ul className="list-decimal list-inside">
+        <h2 className="text-2xl mb-4 text-black">Emparejamientos de la Ronda Actual</h2>
+        <ul className="list-decimal list-inside text-black">
           {matches.map((match, index) => (
             <li key={index} className="mb-2">
-              {match.player1.name} vs {match.player2 ? match.player2.name : 'BYE'}
+              {match.player1} vs {match.player2 || 'BYE'}
             </li>
           ))}
         </ul>
