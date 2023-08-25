4830557f8c5e9608f22ef9f7299dc527e13613f4
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2023-08-24 18:25:14 -0600

    lkldkf

--- components/torneos/MatchupPopup.js
+++ components/torneos/MatchupPopup.js
@@ -1,8 +1,14 @@
-import React, { useState }  from 'react';
+import React, { useState } from 'react';
 import ChatRoom from './ChatRoom';
 
 function MatchupPopup({ matches = [], onClose }) {
-    console.log("Emparejamientos en MatchupPopup:", matches);
+    const [showChat, setShowChat] = useState(false);
+    const [selectedChatRoom, setSelectedChatRoom] = useState(null);
+
+    const openChatRoom = (chatRoomId) => {
+        setSelectedChatRoom(chatRoomId);
+        setShowChat(true);
+    };
 
     function Bracket({ matches }) {
         const totalRounds = Math.log2(matches.length * 2);
@@ -14,11 +20,11 @@ function MatchupPopup({ matches = [], onClose }) {
             </div>
         );
     }
-    
+
     function Round({ matches, round }) {
         const matchesForRound = matches.slice(2 ** round, 2 ** (round + 1));
         return (
-            <div className="round">
+            <div className="round space-y-4">
                 {matchesForRound.map((match, index) => (
                     <Match key={index} match={match} />
                 ))}
@@ -27,36 +33,42 @@ function MatchupPopup({ matches = [], onClose }) {
     }
 
     function Match({ match }) {
+        const isByeMatch = !match.player2;
         return (
-            <div className="match">
-                <div>{match.player1Info ? match.player1Info.username : match.player1}</div>
-                vs
-                <div>{match.player2Info ? match.player2Info.username : (match.player2 || 'BYE')}</div>
+            <div className={`flex items-center space-x-4 ${isByeMatch ? 'bye' : ''}`}>
+                <div className="flex-none w-1/3">
+                    <div className="text-center">
+                        {match.player1Info ? match.player1Info.username : match.player1}
+                    </div>
+                </div>
+                {!isByeMatch && <div className="flex-none w-1/3 text-center">vs</div>}
+                {!isByeMatch && (
+                    <div className="flex-none w-1/3 text-center">
+                        {match.player2Info ? match.player2Info.username : match.player2}
+                    </div>
+                )}
+                <button
+                    className={`px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 ${
+                        match.chatRoom ? 'visible' : 'invisible'
+                    }`}
+                    onClick={() => openChatRoom(match.chatRoom)}
+                >
+                    Chat
+                </button>
             </div>
         );
     }
 
-    const [showChat, setShowChat] = useState(false);
-    const [selectedChatRoom, setSelectedChatRoom] = useState(null);
-  
-    const openChatRoom = (chatRoomId) => {
-      setSelectedChatRoom(chatRoomId);
-      setShowChat(true);
-    };
-  
-    
-    
     return (
         <div className="fixed inset-0 flex items-center justify-center z-50 bg-opacity-50 bg-black">
             <div className="bg-white rounded-lg p-8 w-full md:w-2/3 lg:w-1/2 shadow-lg">
                 <h2 className="text-2xl mb-6 text-black">Emparejamientos de la Ronda Actual</h2>
                 <Bracket matches={matches} />
-
             </div>
             {showChat && (
-        <ChatRoom roomId={selectedChatRoom} onClose={() => setShowChat(false)} />
-      )}
-    </div>
+                <ChatRoom roomId={selectedChatRoom} onClose={() => setShowChat(false)} />
+            )}
+        </div>
     );
 }
 
