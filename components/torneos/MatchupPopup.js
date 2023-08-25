0adfd3fbc3705e2d2bcd4ae0c86e0f4808160ac0
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2023-08-25 01:35:23 -0600

    lkdlfk

--- components/torneos/MatchupPopup.js
+++ components/torneos/MatchupPopup.js
@@ -50,15 +50,18 @@ function MatchupPopup({ matches = [], onClose }) {
                         {match.player2Info ? match.player2Info.username : match.player2}
                     </div>
                 )}
-<button
-    className={`px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600`}
-    onClick={() => openChatRoom(match.chatRoom)}
->
-    Chat
-</button>
+                <div className="flex-none w-1/3">
+                    <button
+                        className={`px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600`}
+                        onClick={() => openChatRoom(match.chatRoom)}
+                    >
+                        Chat
+                    </button>
+                </div>
             </div>
         );
     }
+    
 
     const handleBackdropClick = (event) => {
         if (event.target.classList.contains('backdrop')) {
