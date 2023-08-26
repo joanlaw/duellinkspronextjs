28c01d18b9d454cd4189f836067d17690a9e2371
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2023-08-26 00:32:14 -0600

    lkdlfkl

--- components/torneos/UserMatchupPopup.js
+++ components/torneos/UserMatchupPopup.js
@@ -1,19 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ChatRoom from './ChatRoom';  // Importamos el componente de la sala de chat
+import { useUser  } from '../../contexts/UserContext'; // Importa el contexto del usuario
 
-const UserMatchupPopup = ({ onClose, leagueId, discordId }) => {
+const UserMatchupPopup = ({ onClose, leagueId }) => {
+    const { discordId, token } = useUser();  // Usa useUser para obtener discordId y token
     const [matches, setMatches] = useState([]);
+    const [currentRound, setCurrentRound] = useState(null);
     const [loading, setLoading] = useState(true);
     const [showChat, setShowChat] = useState(false);
     const [selectedChatRoom, setSelectedChatRoom] = useState(null);
 
     useEffect(() => {
+        const fetchCurrentRound = async () => {
+            try {
+                const response = await axios.get(`https://api.duellinks.pro/leagues/${leagueId}/current-round`);
+                setCurrentRound(response.data.current_round);
+            } catch (error) {
+                console.error('Error al obtener la ronda actual:', error);
+            }
+        };
+
         const fetchMatches = async () => {
             setLoading(true);
             try {
-                const response = await axios.get(`https://api.duellinks.pro/leagues/${leagueId}/rounds/current/matches?discordId=${discordId}`);
-                setMatches(response.data);
+                const response = await axios.get(`https://api.duellinks.pro/leagues/${leagueId}/rounds/${currentRound}/match`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,  // Incluye el token en las cabeceras
+                    }
+                });
+                setMatches([response.data]);
             } catch (error) {
                 console.error('Error al obtener los emparejamientos:', error);
             } finally {
@@ -21,8 +37,12 @@ const UserMatchupPopup = ({ onClose, leagueId, discordId }) => {
             }
         };
 
-        fetchMatches();
-    }, [leagueId, discordId]);
+        fetchCurrentRound();
+        if (currentRound) {
+            fetchMatches();
+        }
+
+    }, [leagueId, discordId, currentRound, token]);  // Agrega token y discordId como dependencias
 
     const openChatRoom = (chatRoomId) => {
         setSelectedChatRoom(chatRoomId);
