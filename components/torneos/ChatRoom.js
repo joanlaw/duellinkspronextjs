3b5678c10b95b7e322fa7349a3f0a3b989949181
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2023-08-27 17:08:42 -0600

    lkdlfkl

--- components/torneos/ChatRoom.js
+++ components/torneos/ChatRoom.js
@@ -83,46 +83,50 @@ function ChatRoom({ roomId, onClose }) {
     }
   }
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50 bg-opacity-50 bg-black">
-    <div className="bg-gray-900 rounded-lg p-8 w-full md:w-2/3 lg:w-1/2 shadow-lg">
-      <h2 className="text-2xl mb-6 text-white">
-        Sala de Chat
-      </h2>
-      <div className="h-60 overflow-y-auto border border-gray-300 p-4">
-        {messages.map((message, index) => (
-          <div
-            ref={index === messages.length - 1 ? lastMessageRef : null}
-            key={index}
-            className={`mb-2 p-2 bg-gray-800 text-white rounded-md ${
-              message.sender.discordId === discordId ? 'self-start' : 'self-end'
-            }`}
-          >
-            <strong>{message.sender.username}: </strong> {message.content}
-          </div>
-        ))}
-      </div>
-      <div className="flex mt-4">
-      <Input
-            type="text"
-            value={newMessage}
-            onChange={(e) => setNewMessage(e.target.value)}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                handleSendMessage();
-                e.preventDefault();
-              }
-            }}
-            placeholder="Escribe un mensaje..."
-            width="100%"
-            dark
-          />
-          <Button color="primary" onPress={handleSendMessage}>Enviar</Button>
-          <Button color="primary" onPress={handleClipboard}>Pegar Sala</Button>
-          <Button color="primary" onPress={() => { /* Aquí irá la función para llamar al moderador */ }}>Llamar Mod</Button>
+<div className="fixed inset-0 flex items-center justify-center z-50 bg-opacity-50 bg-black">
+  <div className="bg-gray-900 rounded-lg p-8 w-full md:w-2/3 lg:w-1/2 shadow-lg">
+    <h2 className="text-2xl mb-6 text-white">
+      Sala de Chat
+    </h2>
+    <div className="h-60 overflow-y-auto border border-gray-300 p-4">
+      {messages.map((message, index) => (
+        <div
+          ref={index === messages.length - 1 ? lastMessageRef : null}
+          key={index}
+          className={`mb-2 p-2 bg-gray-800 text-white rounded-md ${
+            message.sender.discordId === discordId ? 'self-start' : 'self-end'
+          }`}
+        >
+          <strong>{message.sender.username}: </strong> {message.content}
         </div>
-        <Button color="danger" onPress={onClose} className="mt-6">Cerrar</Button>
+      ))}
+    </div>
+    <div className="flex mt-4">
+      <Input
+        type="text"
+        value={newMessage}
+        onChange={(e) => setNewMessage(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            handleSendMessage();
+            e.preventDefault();
+          }
+        }}
+        placeholder="Escribe un mensaje..."
+        width="100%"
+        dark
+      />
+      <Button color="primary" onPress={handleSendMessage}>Enviar</Button>
+    </div>
+    <div className="flex justify-between mt-6">
+      <Button color="danger" onPress={onClose}>Cerrar</Button>
+      <div className="space-x-4">
+        <Button color="primary" onPress={handleClipboard}>Pegar Sala</Button>
+        <Button color="primary" onPress={() => { /* Aquí irá la función para llamar al moderador */ }}>Llamar Mod</Button>
       </div>
     </div>
+  </div>
+</div>
   );
 }
 
