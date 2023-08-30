81b0fac46dc9011cd630a3c64a63125585ef48dd
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2023-08-30 17:34:50 -0600

    lkdlfk

--- pages/leagues/[_id].js
+++ pages/leagues/[_id].js
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import TournamentDetails from '../../components/torneos/TournamentDetails';
+import Head from 'next/head';
 
 const TournamentPage = ({ _id }) => {
     const [tournament, setTournament] = useState(null);
@@ -32,8 +33,23 @@ const TournamentPage = ({ _id }) => {
         return <p>Error: {error.message}</p>;
     }
 
-    return <TournamentDetails tournament={tournament} />
-    ;
+    return (
+        <>
+            <Head>
+                <title>{tournament.league_name} - Torneo de Yu-Gi-Oh! Duel Links</title>
+                <meta name="description" content={`Participa en el torneo ${tournament.league_name}. Comienza el ${new Date(tournament.start_date).toLocaleString()}.`} />
+                <meta property="og:title" content={tournament.league_name} />
+                <meta property="og:description" content={`Participa en el torneo ${tournament.league_name}. Comienza el ${new Date(tournament.start_date).toLocaleString()}.`} />
+                <meta property="og:image" content={tournament.image.url} />
+                <meta property="og:type" content="website" />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content={tournament.league_name} />
+                <meta name="twitter:description" content={`Participa en el torneo ${tournament.league_name}. Comienza el ${new Date(tournament.start_date).toLocaleString()}.`} />
+                <meta name="twitter:image" content={tournament.image.url} />
+            </Head>
+            <TournamentDetails tournament={tournament} />
+        </>
+    );
 }
 
 // Este función se usa para obtener los parámetros de la ruta actual
